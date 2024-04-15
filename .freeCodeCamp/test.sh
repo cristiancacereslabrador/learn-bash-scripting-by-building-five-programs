c1475b7267f1b8c20eae4e605bdfc690124f48a3
Cristian Vianey Cáceres Labrador <cristiancacereslabrador@gmail.com>
2024-04-15 21:13:03 +0000

    Save progress: 350.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -4,21 +4,24 @@ echo -e "\n~~ Questionnaire ~~\n"
 
 QUESTION1="What's your name?"
 
+QUESTION2="Where are you from?"
+
+QUESTION3="What's your favorite coding website?"
+
 echo $QUESTION1
 
 read NAME
 
 echo Hello $NAME.
 
-QUESTION2="Where are you from?"
-
 echo $QUESTION2
 
 read LOCATION
 
+echo $QUESTION3
+
+read WEBSITE
+
 echo Hello $NAME from $LOCATION.
 
-QUESTION3="What's your favorite coding website?"
-
-
-echo $QUESTION3
+echo $WEBSITE
--- questionnaire.sh
+++ questionnaire.sh
@@ -7,17 +7,19 @@ QUESTION1="What's your name?"
 QUESTION2="Where are you from?"
 
 QUESTION3="What's your favorite coding website?"
+
 echo $QUESTION1
 
 read NAME
 
 echo Hello $NAME.
 
-
 echo $QUESTION2
 
 read LOCATION
 
 echo $QUESTION3
+
+read WEBSITE
 
 echo Hello $NAME from $LOCATION.
