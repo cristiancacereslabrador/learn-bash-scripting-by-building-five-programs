c025c97d31f6542d869764abaf67657c9fe4aedf
Cristian Vianey Cáceres Labrador <cristiancacereslabrador@gmail.com>
2024-04-16 16:22:10 +0000

    Save progress: 950.1

--- bingo.sh
+++ bingo.sh
@@ -4,7 +4,11 @@
 
 echo -e "\n~~ Bingo Number Generator ~~\n"
 
-NUMBER=$((RANDOM % 75+1))
-echo $NUMBER
+NUMBER=$((RANDOM % 75 + 1))
 
 TEXT="The next number is, "
+
+if (( NUMBER <= 15 ))
+then
+  echo $TEXT B:$NUMBER
+fi
